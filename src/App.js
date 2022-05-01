ffe5f0c7d7d11c450e12b3b122a66d80f2d12677
crosspool03@gmail.com <crosspool03@gmail.com>
2022-05-01 13:04:12 -0300

    up

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home } from "./components/Home";
 import { Categories } from "./components/Categories";
 import { Search } from "./components/Search";
+import { Details } from "./components/Details";
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/search/:searched" element={<Search />} />
+        <Route path="/:details" element={<Details />} />
         <Route path="/Categories" element={<Categories />} />
       </Routes>
     </Router>
