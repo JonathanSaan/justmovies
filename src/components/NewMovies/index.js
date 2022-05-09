9226d04c22d3b0cc1ccef1e5bd120937287a0a4b
crosspool03@gmail.com <crosspool03@gmail.com>
2022-05-09 20:00:16 -0300

    up

---
+++ src/components/NewMovies/index.js
@@ -0,0 +1,18 @@
+import { Header } from "../Header";
+import "./style.scss"
+
+
+export const NewMovies = () => {
+  
+  return (
+      <>
+        <Header />
+        <div className="Container">
+          <div>
+            <h1>Page in developing. </h1>
+          </div>
+        </div>
+      </>
+    );
+    
+};
---
+++ src/components/TopRated/index.js
@@ -0,0 +1,18 @@
+import { Header } from "../Header";
+import "./style.scss"
+
+
+export const TopRated = () => {
+  
+  return (
+      <>
+        <Header />
+        <div className="Container">
+          <div>
+            <h1>Page in developing. </h1>
+          </div>
+        </div>
+      </>
+    );
+    
+};
