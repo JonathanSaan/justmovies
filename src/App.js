b599379a265cd2032cd47f787b0cc5d0f8919d54
Jonathan <crosspool03@gmail.com>
2022-05-09 19:49:45 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import { Home } from "./components/Home";
+import { NewMovies } from "./components/NewMovies";
+import { TopRated } from "./components/TopRated";
 import { Categories } from "./components/Categories";
 import { Search } from "./components/Search";
 import { Details } from "./components/Details";
@@ -11,6 +13,8 @@ function App() {
     <Router>
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route exact path="/NewMovies" element={<NewMovies />} />
+        <Route exact path="/TopRated" element={<TopRated />} />
         <Route path="/search/:searched" element={<Search />} />
         <Route path="/:details" element={<Details />} />
         <Route path="/Categories" element={<Categories />} />
