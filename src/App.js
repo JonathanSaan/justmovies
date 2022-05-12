037aafb3a229f460520a11a1831af4adaa08904f
Jonathan <crosspool03@gmail.com>
2022-05-12 20:34:15 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { SkeletonTheme } from "react-loading-skeleton";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import { Home } from "./components/Home";
@@ -10,16 +11,18 @@ import { Details } from "./components/Details";
 function App() {
 
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/NewMovies" element={<NewMovies />} />
-        <Route exact path="/TopRated" element={<TopRated />} />
-        <Route path="/search/:searched" element={<Search />} />
-        <Route path="/:details" element={<Details />} />
-        <Route path="/Categories" element={<Categories />} />
-      </Routes>
-    </Router>
+    <SkeletonTheme baseColor="#202020" highlightColor="#444">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/NewMovies" element={<NewMovies />} />
+          <Route exact path="/TopRated" element={<TopRated />} />
+          <Route path="/search/:searched" element={<Search />} />
+          <Route path="/:details" element={<Details />} />
+          <Route path="/Categories" element={<Categories />} />
+        </Routes>
+      </Router>
+    </SkeletonTheme>
   );
   
 };
