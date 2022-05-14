25209978decab5c991d28c699f859e41e6aaac3d
crosspool03@gmail.com <crosspool03@gmail.com>
2022-05-14 18:03:22 -0300

    up

--- src/components/NewMovies/index.js
+++ src/components/NewMovies/index.js
@@ -1,15 +1,48 @@
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+import APIKey from "../../mocks/api";
 import { Header } from "../Header";
 import "./style.scss"
 
 
 export const NewMovies = () => {
   
+  const [ listMovies, setListMovies ] = useState([])
+  
+  useEffect(() => { 
+    window.scrollTo(0, 0);
+    const load = async () => {
+      try {
+        const respost = await axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${APIKey}&language=en-US&page=1`);
+        setListMovies(respost.data.results);
+      } catch (error) {
+        console.log(error);
+      };
+    };
+    load() 
+  }, [] );
+  
+  const Image_path = "https://image.tmdb.org/t/p/w500";
+  const imageError = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNHowX2RIOXDQtQ6EWW7zJ_RC8xhiSsXNihA&usqp=CAU";
+  
   return (
       <>
         <Header />
-        <div className="Container">
-          <div>
-            <h1>Page in developing. </h1>
+        <div className="ContainerMovies">
+          <div className="NewMovie">
+            <div className="title">
+              <h1>New Movies</h1>
+            </div>
+            {listMovies.map((movie) => (
+              <Link to={`/${movie.id}`}>
+                <div className="Movies" key={movie.id}>
+                  <img className="MovieImage" src={movie.poster_path ? Image_path + movie.poster_path : imageError} alt={movie.title} /> 
+                  <h2 className="MovieTitle">{movie.title}</h2>
+                </div>
+              </Link>
+            ))}
           </div>
         </div>
       </>
--- src/components/TopRated/index.js
+++ src/components/TopRated/index.js
@@ -1,15 +1,48 @@
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+import APIKey from "../../mocks/api";
 import { Header } from "../Header";
 import "./style.scss"
 
-
 export const TopRated = () => {
+  
+  const [ listRated, setListRated ] = useState([])
+  
+  useEffect(() => { 
+    window.scrollTo(0, 0);
+    const load = async () => {
+      try {
+        const respost = await axios.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=${APIKey}&language=en-US&page=1`);
+        setListRated(respost.data.results);
+      } catch (error) {
+        console.log(error);
+      };
+    };
+    
+    load() 
+  }, [] );
+  
+  const Image_path = "https://image.tmdb.org/t/p/w500";
+  const imageError = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNHowX2RIOXDQtQ6EWW7zJ_RC8xhiSsXNihA&usqp=CAU";
   
   return (
       <>
         <Header />
-        <div className="Container">
-          <div>
-            <h1>Page in developing. </h1>
+        <div className="ContainerRated">
+          <div className="TopRated">
+            <div className="title">
+              <h1>New Movies</h1>
+            </div>
+            {listRated.map((rated) => (
+              <Link to={`/${rated.id}`} >
+                <div className="Rateds" key={rated.id}>
+                  <img className="RatedImage" src={rated.poster_path ? Image_path + rated.poster_path : imageError} alt={rated.name}/>
+                  <h2 className="RatedTitle">{rated.title}</h2>
+                </div>
+              </Link>
+            ))}
           </div>
         </div>
       </>
