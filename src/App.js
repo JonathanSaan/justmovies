dce25b7e27ea6008cfed2f70f23a0d5ef7693359
Jonathan <crosspool03@gmail.com>
2022-05-17 15:22:39 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Home } from "./components/Home";
 import { NewMovies } from "./components/NewMovies";
 import { TopRated } from "./components/TopRated";
 import { Categories } from "./components/Categories";
+import { MoviesGenre } from "./components/MoviesGenre";
 import { Search } from "./components/Search";
 import { Details } from "./components/Details";
 
@@ -17,9 +18,11 @@ function App() {
           <Route exact path="/" element={<Home />} />
           <Route exact path="/NewMovies" element={<NewMovies />} />
           <Route exact path="/TopRated" element={<TopRated />} />
+          <Route path="/search/" element={<Home />} />
           <Route path="/search/:searched" element={<Search />} />
           <Route path="/:details" element={<Details />} />
-          <Route path="/Categories" element={<Categories />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/categories/:genre" element={<MoviesGenre />} />
         </Routes>
       </Router>
     </SkeletonTheme>
