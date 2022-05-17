95029ad29298260cad45814d0df64b216332dd97
Jonathan <crosspool03@gmail.com>
2022-05-17 15:23:06 -0300

    Add files via upload

--- src/components/Categories/index.js
+++ src/components/Categories/index.js
@@ -1,18 +1,48 @@
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+
 import { Header } from "../Header";
 import "./style.scss"
 
+import APIKey from "../../mocks/api";
+
 
 export const Categories = () => {
+  
+  const [ categorie, setCategorie ] = useState([])
+  
+  useEffect(() => { 
+    window.scrollTo(0, 0);
+    const load = async () => {
+      try {
+        const respost = await axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${APIKey}&language=en-US`);
+        setCategorie(respost.data.genres);
+        
+      } catch (error) {
+        console.log(error);
+      };
+    };
+    load() 
+  }, [] );
   
   return (
       <>
         <Header />
         <div className="Categories">
-          <div>
-            <h1>Page in developing. </h1>
+          <div className="ContainerCategorie">
+            <div className="title">
+              <h1>Categories</h1>
+            </div>
+            {categorie.map((genre) => (
+              <Link to={`/Categories/${genre.name.replaceAll(" ", "-")}`}>
+                <button> 
+                  {genre.name}
+                </button>
+              </Link>
+            ))}
           </div>
         </div>
       </>
     );
-    
 };
