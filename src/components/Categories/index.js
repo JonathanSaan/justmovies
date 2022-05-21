56702b3127f23ccf47ec117c0563d461342fd594
Jonathan <crosspool03@gmail.com>
2022-05-21 12:24:38 -0300

    Add files via upload

--- src/components/Categories/index.js
+++ src/components/Categories/index.js
@@ -8,41 +8,25 @@ import "./style.scss"
 import APIKey from "../../mocks/api";
 
 
-export const Categories = () => {
-  
-  const [ category, setCategories ] = useState([])
-  
-  useEffect(() => { 
-    window.scrollTo(0, 0);
-    const load = async () => {
-      try {
-        const respost = await axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${APIKey}&language=en-US`);
-        setCategories(respost.data.genres);
-        
-      } catch (error) {
-        console.log(error);
-      };
-    };
-    load() 
-  }, [] );
+export const Categories = ({ category, setIdGenreSelected }) => {
   
   return (
-      <>
-        <Header />
-        <div className="Categories">
-          <div className="ContainerCategory">
-            <div className="title">
-              <h1>Category</h1>
-            </div>
-            {category.map((genre) => (
-              <Link to={`/categories/${genre.name.replaceAll(" ", "-")}`}>
-                <button> 
-                  {genre.name}
-                </button>
-              </Link>
-            ))}
+    <>
+      <Header />
+      <div className="Categories">
+        <div className="ContainerCategory">
+          <div className="title">
+            <h1>Category</h1>
           </div>
+          {category.map((genre) => (
+            <Link onClick={() => setIdGenreSelected(genre.id)} to={`/categories/${genre.name.replaceAll(" ", "-").toLowerCase()}`}>
+              <button> 
+                {genre.name}
+              </button>
+            </Link>
+          ))}
         </div>
-      </>
-    );
+      </div>
+    </>
+  );
 };
