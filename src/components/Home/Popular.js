066be4507e4328e4e250fe86f1f061da5e219789
JonathanSaan <jonathansaandev>
2022-12-06 20:08:44 +0000

    up

--- src/components/Home/Popular.js
+++ src/components/Home/Popular.js
@@ -1,25 +1,28 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 import axios from "axios";
+import { motion } from "framer-motion";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
-import Whirligig from "react-whirligig";
 
 import APIKey from "../../mocks/api";
 
 
 
 export const Popular = ({ navigate }) => {
-  
-  const [ listPopular, setListPopular ] = useState([])
-  
+  const carousel = useRef();
+  const [width, setWidth] = useState(0);
+
+  const [ listPopular, setListPopular ] = useState([]);
+
   useEffect(() => { 
     window.scrollTo(0, 0);
+    setWidth(carousel.current?.scrollWidth - carousel.current?.offsetWidth);
     const load = async () => {
       const respost = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${APIKey}&language=en-US&page=1`);
       setListPopular(respost.data.results);
     };
-    load() 
+    load();
   }, [] );
   
   const Image_path = "https://image.tmdb.org/t/p/w500";
@@ -30,23 +33,36 @@ export const Popular = ({ navigate }) => {
  return (
    <>
     {!listPopular.length == 0 ? (
-      <Whirligig visibleSlides={6} gutter="1em">
-        {listPopular.map((popular) => (
-            <div onClick={() => {navigate(`/${popular.id}`) }} className="Populars" key={popular.id}>
-              <img className="PopularImage" src={popular.poster_path ? Image_path + popular.poster_path : imageError} alt={popular.title} /> 
-              <h2 className="PopularTitle">{popular.title}</h2>
-            </div>
-          ))}
-      </Whirligig>
+      <motion.div
+        className="carousel"
+        ref={carousel}
+        whileTap={{ cursor: "grabbing" }}
+      >
+        <motion.div
+          className="inner"
+          drag="x"
+          dragConstraints={{ right: 0, left: -width }}
+          initial={{ x: 100 }}
+          animate={{ x: 0 }}
+          transition={{ duration: 0.8 }}
+        >
+          {listPopular.map((popular) => (
+              <div onClick={() => {navigate(`/${popular.id}`) }} className="Populars" key={popular.id}>
+                <img className="PopularImage" src={popular.poster_path ? Image_path + popular.poster_path : imageError} alt={popular.title} /> 
+                <h2 className="PopularTitle">{popular.title}</h2>
+              </div>
+            ))}
+        </motion.div>
+      </motion.div>
       ) : (
-        <Whirligig visibleSlides={6} gutter="1em">
+        <motion.div>
           {Array(20).fill(1).map((card, index) => (
             <>
               <Skeleton className="ImageLoading" variant="rectangular" />
               <Skeleton className="Text" variant="text" count={1}/>
             </>
           ))}
-        </Whirligig>
+        </motion.div>
       )}
    </>
   );
