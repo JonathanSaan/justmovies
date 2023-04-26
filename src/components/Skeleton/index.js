fe0daca5d9b5f7dbd54db2ae37bcad3796b75e26
JonathanSaan <crosspool03@gmail.com>
2023-04-26 19:49:03 -0300

    skeleton

--- src/components/Skeleton/index.js
+++ src/components/Skeleton/index.js
@@ -3,13 +3,11 @@ import Slider from "react-slick";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
-
-
 export const SkeletonHomePopular = ({ popularcarouselsetting }) => {
     return (
         <Slider {...popularcarouselsetting} className="home_container_popular">
-            {Array(20).fill(1).map((card, index) => (
-                <div key={index} className="home_container_popular_card">
+            {Array(20).fill(1).map(() => (
+                <div className="home_container_popular_card">
                     <Skeleton className="home_container_popular_card-image" variant="rectangular" />
                     <Skeleton className="home_container_popular_card-title" variant="text" count={1} />
                 </div>
@@ -20,8 +18,8 @@ export const SkeletonHomePopular = ({ popularcarouselsetting }) => {
 export const SkeletonHomeMovies = () => {
     return (
         <>
-            {Array(12).fill(1).map((card, index) => (
-                <div key={index} className="home_container_newmovie_card">
+            {Array(12).fill(1).map(() => (
+                <div className="home_container_newmovie_card">
                     <Skeleton className="home_container_newmovie_card-image" variant="rectangular" />
                     <Skeleton className="home_container_newmovie_card-title" variant="text" count={1} />
                 </div>
@@ -32,8 +30,8 @@ export const SkeletonHomeMovies = () => {
 export const SkeletonHomeTopRated = () => {
     return (
         <>
-            {Array(12).fill(1).map((card, index) => (
-                <div key={index} className="home_container_ratedmovie_card">
+            {Array(12).fill(1).map(() => (
+                <div className="home_container_ratedmovie_card">
                     <Skeleton className="home_container_ratedmovie_card-image" variant="rectangular" />
                     <Skeleton className="home_container_ratedmovie_card-title" variant="text" count={1} />
                 </div>
@@ -46,7 +44,7 @@ export const SkeletonHomeTopRated = () => {
 export const SkeletonMoviesGenre = () => {
     return (
         <>
-            {Array(18).fill(1).map((card, index) => (
+            {Array(18).fill(1).map(() => (
                 <div className="genre_containergenre_card">
                     <Skeleton className="genre_containergenre_card-image" variant="rectangular" />
                     <Skeleton className="genre_containergenre_card-title" variant="text" count={1} />
@@ -59,7 +57,7 @@ export const SkeletonMoviesGenre = () => {
 export const SkeletonNewMovies = () => {
     return (
         <>
-            {Array(18).fill(1).map((card, index) => (
+            {Array(18).fill(1).map(() => (
                 <div className="newmovies_container_card">
                     <Skeleton className="newmovies_container_card-image" variant="rectangular" />
                     <Skeleton className="newmovies_container_card-title" variant="text" count={1} />
@@ -72,7 +70,7 @@ export const SkeletonNewMovies = () => {
 export const SkeletonTopRated = () => {
     return (
         <>
-            {Array(18).fill(1).map((card, index) => (
+            {Array(18).fill(1).map(() => (
                 <div className="ratedmovie_container_card">
                     <Skeleton className="ratedmovie_container_card-image" variant="rectangular" />
                     <Skeleton className="ratedmovie_container_card-title" variant="text" count={1} />
