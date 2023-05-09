f7903be0252bcba6f537ce6c443ef3d59890e5dc
jonathansaandev <jonathansaandev@gmail.com>
2023-05-08 23:45:21 -0300

    skeleton movie details

--- src/components/Skeleton/index.js
+++ src/components/Skeleton/index.js
@@ -1,9 +1,13 @@
-import Slider from "react-slick";
+import { useState } from "react";
 
+import Slider from "react-slick";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 
-export const SkeletonHomePopular = ({ popularcarouselsetting }) => {
+import { popularcarouselsetting, detailscarouselsetting } from "../../mocks/carouselsettings";
+
+export const SkeletonHomePopular = () => {
     return (
         <Slider {...popularcarouselsetting} className="home_container_popular">
             {Array(20).fill(1).map(() => (
@@ -78,4 +82,74 @@ export const SkeletonTopRated = () => {
             ))}
         </>
     );
+};
+
+
+export const SkeletonMovieDetails = () => {
+	const [activeTab, setActiveTab] = useState("tab1");
+	const handleTab = (index) => {
+    	setActiveTab(`tab${index}`);
+  	};
+  	
+	return (
+		<div className="details_container">
+          <div className="details_container_movie">
+            <Skeleton className="details_container_movie-image" variant="rectangular" />
+
+            <span className="details_container_movie-details">
+              <Skeleton className="details_container_movie-details-title" variant="text" count={1} />
+              <Skeleton className="details_container_movie-details-releasedate" variant="text" count={1} />
+            </span>
+          </div>
+
+          <div className="details_container_movie-synopsis">
+            <hr />
+            <Skeleton className="details_container_movie-synopsis-paragraph" variant="text" count={7} />
+          </div>
+
+          <div className="details_container_movie-containervideo">
+            <Skeleton className="details_container_movie-containervideo-video" variant="rectangular" />
+          </div>
+
+          <div className="AllTab">
+            <Tabs defaultIndex={0} className="Tabs">
+              <TabList className="TabList">
+                <Tab
+                  onClick={() => handleTab(1)}
+                  className={activeTab === "tab1" ? "active" : "false"}
+                >
+                  Characters
+                </Tab>
+                <Tab
+                  onClick={() => handleTab(2)}
+                  className={activeTab === "tab2" ? "active" : "false"}
+                >
+                  Similar
+                </Tab>
+              </TabList>
+              <TabPanel className="TabPanel">
+                  <Slider {...detailscarouselsetting} className="carousel1">
+                    {Array(20).fill(1).map((card, index) => (
+                      <div className="item">
+                        <Skeleton className="ImageLoading" variant="rectangular" />
+                        <Skeleton className="Text" variant="text" count={1} />
+                      </div>
+                    ))}
+                  </Slider>
+              </TabPanel>
+
+              <TabPanel className="TabPanel">
+                  <Slider {...detailscarouselsetting} className="carousel1">
+                    {Array(20).fill(1).map((card, index) => (
+                      <div className="item">
+                        <Skeleton className="ImageLoading" variant="rectangular"/>
+                        <Skeleton className="Text" variant="text" count={1} />
+                      </div>
+                    ))}
+                  </Slider>
+              </TabPanel>
+            </Tabs>
+          </div>
+        </div>
+	);
 };
