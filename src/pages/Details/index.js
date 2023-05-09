d2407ee78439791a4dee32235326f651e93cea79
jonathansaandev <jonathansaandev@gmail.com>
2023-05-08 23:41:13 -0300

    details

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -3,8 +3,6 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams, Link } from "react-router-dom";
 import { Helmet } from "react-helmet";
-import Skeleton from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
 import { IoIosStar } from "react-icons/io";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
@@ -13,6 +11,7 @@ import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 
 import APIKey from "../../mocks/api";
 import { detailscarouselsetting } from "../../mocks/carouselsettings";
+import { SkeletonMovieDetails } from "../../components/Skeleton";
 import Header from "../../components/Header";
 import "./style.scss";
 
@@ -24,40 +23,48 @@ const Details = () => {
   const [genres, setGenres] = useState([]);
   const [trailer, setTrailer] = useState([]);
 
-  const [activeTab, setActiveTab] = useState("tab1");
-  const handleTab1 = () => {
-    setActiveTab("tab1");
-  };
-  const handleTab2 = () => {
-    setActiveTab("tab2");
-  };
-
   const [characters, setCharacters] = useState([]);
   const [movieSimilar, setMovieSimilar] = useState([]);
-
-  const [loading, setLoading] = useState(false);
-
+  
+  const [activeTab, setActiveTab] = useState("tab1");
+  const handleTab = (index) => {
+    setActiveTab(`tab${index}`);
+  };
+  
+   const resetComponents = () => {
+    window.scrollTo({
+  	  top: 0,
+  	  left: 0,
+  	  behavior: 'smooth'
+	});
+    setDetailsMovie([]);
+  	setGenres([]);
+  	setYearMovie([]);
+  	setTrailer([]);
+  	setCharacters([]);
+  	setMovieSimilar([]);
+  	setActiveTab("tab1");
+  };
+  
   useEffect(() => {
-    window.scrollTo(0, 0);
-    
     const load = async () => {
+      resetComponents();
       const respost = await axios.get(`https://api.themoviedb.org/3/movie/${details}?api_key=${APIKey}&language=en-US`);
       setDetailsMovie(respost.data);
       setGenres(respost.data.genres);
       setYearMovie(respost.data.release_date.slice(0, 4));
-
+		
       const videos = await axios.get(`https://api.themoviedb.org/3/movie/${details}/videos?api_key=${APIKey}&language=en-US&append_to_response=videos`);
       
       if (videos.data && videos.data.results && videos.data.results[0]) {
         setTrailer(videos.data.results[0]);
       }
-
-      const dataSimilar = await axios.get(`https://api.themoviedb.org/3/movie/${details}/similar?api_key=${APIKey}&language=en-US&page=1`);
-      setMovieSimilar(dataSimilar.data.results);
-
+      
       const credits = await axios.get(`https://api.themoviedb.org/3/movie/${details}/credits?api_key=${APIKey}&language=en-US`);
       setCharacters(credits.data.cast);
-      setLoading(true);
+      
+      const dataSimilar = await axios.get(`https://api.themoviedb.org/3/movie/${details}/similar?api_key=${APIKey}&language=en-US&page=1`);
+      setMovieSimilar(dataSimilar.data.results);
     };
     load();
   }, [details]);
@@ -77,24 +84,19 @@ const Details = () => {
           style={{ backgroundImage: `url(${imagePath}${detailsMovie.backdrop_path})`}}
         ></div>
 
+        {Object.keys(detailsMovie).length > 0 ? ( 
         <div className="details_container">
           <div className="details_container_movie">
-            {detailsMovie.poster_path && (
-              <img
-                loading="lazy"
-                className="details_container_movie-image"
-                src={detailsMovie.poster_path ? imagePath + detailsMovie.poster_path : imageError}
-                alt={detailsMovie.title}
-              />
-            )}
-
-            {!detailsMovie.poster_path && !loading && (
-              <Skeleton className="details_container_movie-image" variant="rectangular" />
-            )}
+            <img
+              loading="lazy"
+              className="details_container_movie-image"
+              src={detailsMovie.poster_path ? imagePath + detailsMovie.poster_path : imageError}
+              alt={detailsMovie.title}
+            />
 
             <span className="details_container_movie-details">
               <h1 className="details_container_movie-details-title">
-                {detailsMovie.title || <Skeleton variant="text" count={2} />}
+                {detailsMovie.title}
               </h1>
               <p className="details_container_movie-details-releasedate">
                 {yearMovie}
@@ -125,10 +127,8 @@ const Details = () => {
           </div>
 
           <div className="details_container_movie-synopsis">
-            <hr />
-
             <h2 className="details_container_movie-synopsis-paragraph">
-              {detailsMovie.overview || <Skeleton variant="text" count={7} />}
+              {detailsMovie.overview}
             </h2>
           </div>
 
@@ -144,30 +144,25 @@ const Details = () => {
                 allowfullscreen
               ></iframe>
             )}
-
-            {!trailer.key && !loading && (
-              <Skeleton className="details_container_movie-containervideo-video" variant="rectangular" />
-            )}
           </div>
 
           <div className="AllTab">
             <Tabs defaultIndex={0} className="Tabs">
               <TabList className="TabList">
                 <Tab
-                  onClick={handleTab1}
+                  onClick={() => handleTab(1)}
                   className={activeTab === "tab1" ? "active" : "false"}
                 >
                   Characters
                 </Tab>
                 <Tab
-                  onClick={handleTab2}
+                  onClick={() => handleTab(2)}
                   className={activeTab === "tab2" ? "active" : "false"}
                 >
                   Similar
                 </Tab>
               </TabList>
               <TabPanel className="TabPanel">
-                {characters.length > 0 ? (
                   <Slider {...detailscarouselsetting} className="carousel1">
                     {characters.map((Character) => (
                       <div className="item" key={Character.id}>
@@ -180,49 +175,30 @@ const Details = () => {
                       </div>
                     ))}
                   </Slider>
-                ) : (
-                  <Slider {...detailscarouselsetting} className="carousel1">
-                    {Array(20).fill(1).map((card, index) => (
-                      <div className="item">
-                        <Skeleton className="ImageLoading" variant="rectangular" />
-                        <Skeleton className="Text" variant="text" count={1} />
-                      </div>
-                    ))}
-                  </Slider>
-                )}
               </TabPanel>
 
               <TabPanel className="TabPanel">
-                {movieSimilar.length > 0 ? (
                   <Slider {...detailscarouselsetting} className="carousel2">
                     {movieSimilar.map((similar) => (
-                      <Link to={`/${similar.id}`}
-                        className="item"
-                        key={similar.id}
-                      >
-                        <img
-                          src={similar.poster_path ? imagePath + similar.poster_path : imageError}
-                          loading="lazy"
-                          alt={similar.title}
-                        />
-                        <p>{similar.title}</p>
-                      </Link>
-                    ))}
-                  </Slider>
-                ) : (
-                  <Slider {...detailscarouselsetting} className="carousel1">
-                    {Array(20).fill(1).map((card, index) => (
-                      <div className="item">
-                        <Skeleton className="ImageLoading" variant="rectangular"/>
-                        <Skeleton className="Text" variant="text" count={1} />
+                      <div className="item" key={similar.id}>
+                      	<Link to={`/${similar.id}`}>
+                          <img
+                            src={similar.poster_path ? imagePath + similar.poster_path : imageError}
+                            loading="lazy"
+                            alt={similar.title}
+                          />
+                        </Link>
+                        <Link to={`/${similar.id}`}>
+                          <p>{similar.title}</p>
+                        </Link>
                       </div>
                     ))}
                   </Slider>
-                )}
               </TabPanel>
             </Tabs>
           </div>
         </div>
+        ) : <SkeletonMovieDetails /> }
       </div>
     </>
   );
