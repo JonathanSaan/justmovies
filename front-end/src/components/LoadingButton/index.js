f82027c747c1770961d709ab36e2e6501f3a3949
jonathansaandev <jonathansaandev@gmail.com>
2023-11-29 00:34:18 -0300

    Fix LoadingButton component to disable button when loading

--- front-end/src/components/LoadingButton/index.js
+++ front-end/src/components/LoadingButton/index.js
@@ -2,13 +2,13 @@ import { CircularProgress } from "react-cssfx-loading";
 
 const LoadingButton = ({ event, styleButton, loading, message }) => {
   return (
-    <button onClick={event ? event : null} type="submit" className={styleButton} >
-    	{loading ? <Icone event={event} /> : message}
+    <button onClick={event ? event : null} type="submit" disabled={loading} className={styleButton} >
+    	{loading ? <Icon event={event} /> : message}
     </button>
   );
 };
 
-const Icone = ({ event }) => { 
+const Icon = ({ event }) => { 
   return ( 
   	<CircularProgress color={event ? "#808080" : "#000000"} height="2em" width="2em" /> 
   )
