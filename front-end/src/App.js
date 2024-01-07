eafd91184d37fe92472b63f9860b2971937bb1d0
jonathansaandev <jonathansaandev@gmail.com>
2024-01-07 01:12:37 -0300

    Update routes to redirect logged-in users

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,6 @@
 import { Analytics } from "@vercel/analytics/react";
 import { SkeletonTheme } from "react-loading-skeleton";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Error from "./pages/Error";
@@ -21,6 +21,8 @@ import ResetPassword from "./pages/ResetPassword";
 import PrivateRoute from "./privateRoute";
 
 function App() {
+  const isLoggedIn = localStorage.getItem("user") || sessionStorage.getItem("user");
+
   return (
     <SkeletonTheme baseColor="#202020" highlightColor="#444">
       <Router>
@@ -35,9 +37,9 @@ function App() {
               </PrivateRoute>
             }
           />
-          <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/recovery" element={<Recovery />} />
+          <Route path="/sign-in" element={isLoggedIn ? <Navigate to="/" /> : <SignIn />} />
+          <Route path="/sign-up" element={isLoggedIn ? <Navigate to="/" /> : <SignUp /> } />
+          <Route path="/recovery" element={isLoggedIn ? <Navigate to="/" /> : <Recovery />} />
           <Route path="/apps" element={<Apps />} />
           <Route path="/new-movies" element={<NewMovies />} />
           <Route path="/new-movies?page=:page" element={<NewMovies />} />
@@ -48,7 +50,7 @@ function App() {
           <Route path="/movies/:details" element={<MovieDetails />} />
           <Route path="/genre/:id/:genre" element={<MoviesGenre />} />
           <Route path="/genre/:id/:genre?page=:page" element={<MoviesGenre />} />
-          <Route path="/reset-password/id=:id/token=:token" element={<ResetPassword />} />
+          <Route path="/reset-password/id=:id/token=:token" element={isLoggedIn ? <Navigate to="/" /> : <ResetPassword />} />
           <Route path="*" element={<Error />} />
         </Routes>
       </Router>
