8c172d849a14453ea128ec2b8354b30b2f6f5fae
jonathansaandev <jonathansaandev@gmail.com>
2024-03-29 15:48:55 -0300

    Fix cache response to include username and data

--- back-end/src/middlewares/global.middlewares.js
+++ back-end/src/middlewares/global.middlewares.js
@@ -35,23 +35,23 @@ export const validUser = async (req, res, next) => {
   }
 };
 
-export const cache = async (req, res, next) => {
-  try {
-    const username = req.params.username;
-  
-    client.get(username, (err, data) => {
-      if (err) throw err;
-    
-      if (data !== null) {
-        console.log("Data is already cached");
-        res.send(JSON.parse(data));
-        return;
-      } 
-    
-      console.log("Data is not cached");
-      next();
-    });
-  } catch (err) {
-    res.status(500).send({ message: err.message });
-  }
+export const cache = (req, res, next) => {
+  const username = req.params.username;
+
+  client.get(username, (err, data) => {
+    if (err) {
+      console.error("Error fetching from cache:", err);
+      res.status(500).send({ message: err.message });
+      return;
+    }
+
+    if (data !== null) {
+      console.log("Data is already cached");
+      res.send(JSON.parse(username, data));
+      return;
+    } 
+
+    console.log("Data is not cached, calling next()");
+    next();
+  });
 };
