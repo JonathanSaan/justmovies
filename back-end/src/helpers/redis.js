126abb26beedd6917e8331a1865904f37987df3c
jonathansaandev <jonathansaandev@gmail.com>
2024-04-03 22:29:12 -0300

    Add error handling for Redis connection

--- back-end/src/helpers/redis.js
+++ back-end/src/helpers/redis.js
@@ -6,4 +6,8 @@ client.on("connect", () => {
   console.log("Redis Connected!");
 });
 
+client.on("error", (error) => {
+  console.log(`Error connecting to Redis: ${error}`);
+});
+
 export default client;
