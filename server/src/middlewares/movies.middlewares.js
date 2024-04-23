7a868946cae3e456643a98b227c28428334f1f2e
jonathansaandev <jonathansaandev@gmail.com>
2024-04-23 15:26:11 -0300

    Update moviesMiddleware to use async/await for axios request

--- server/src/middlewares/movies.middlewares.js
+++ server/src/middlewares/movies.middlewares.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const moviesMiddleware = (req, res, next) => {
+const moviesMiddleware = async (req, res, next) => {
   try {
-    const response = axios.get(`https://api.themoviedb.org/3/movie/${req.params.movieId}?api_key=${process.env.API_TMDB}&language=en-US"`);
+    const response = await axios.get(`https://api.themoviedb.org/3/movie/${req.params.movieId}?api_key=${process.env.API_TMDB}&language=en-US"`);
 
     if (!response) {
       res.status(400).send({ message: "This movie does not exist in our catalog." });
